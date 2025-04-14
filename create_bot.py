79fc7f230a428687954233a0fe5769a54c2a6239
Shubhranka <shubhranka.varma@sourcefuse.com>
2025-04-14 17:45:41 +0530

    added health check and req.txt

--- create_bot.py
+++ create_bot.py
@@ -92,5 +92,9 @@ def train_model():
             "details": e.stderr
         }), 500
 
+@app.route('/health', methods=['GET'])
+def health():
+    return jsonify({"status": "OK"}), 200
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0")
